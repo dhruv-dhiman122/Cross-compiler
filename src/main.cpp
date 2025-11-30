a182d80579aa2f8fa0a3da760e618087e06fb37e
dhruv dhiman <dhruvdhiman770@gmail.com>
2025-11-30 14:15:18 +0530

    the src dict is made with main file in it

---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+
+/*
+ * Aim of file -> To connect all other file that are made in this repo
+ * */
+
+
+#include <iostream>
+#include <cstdlib>
+#include <unistd.h>
+#include <stdint.h>
+
+
+
+//================================================= Space only for user defined macro or golbal variables ================================//
+
+#define EXT_SUCCESS 0
+#define EXT_FAILURE 1
+
+//================================================= Space for user defined function only ========================================//
+
+
+
+//================================================== Spaace for main funtion only =================================================//
+int main() {
+
+
+    exit(EXT_SUCCESS);
+}
